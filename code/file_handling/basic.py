00132f9f92ecad2d520e2e4c1926b8faa6712f68
GR SURYANARAYANA <suryagr14@gmail.com>
2024-06-12 14:27:12 +0530

    Internship day 3

---
+++ code/file_handling/basic.py
@@ -0,0 +1,8 @@
+import os
+with open("demo.txt","a") as file:
+  file.write("\nwelcome dude")
+  file.close()
+with open("demo.txt","r") as file:
+   for i in file.readlines():
+      print(i,end=" ")
+
